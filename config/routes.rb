75f57a156a3e9967262ff585571d20909bbc8f7e
Nicolas Saenko <nicolas.ozz.com@gmail.com>
2016-09-22 16:22:23 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'application#angular'
+
+  resources :projects, only: [:create, :update, :destroy, :index] do
+    resources :tasks, only: [:create, :update, :destroy, :show] do
+      resources :comments, only: [:create, :update, :destroy]
+    end
+  end
 end
